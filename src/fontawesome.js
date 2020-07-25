2c4f37d7cadfb483d4676b09edfd44b1e350f1ed
Olumuyiwa <oluosiname@gmail.com>
2020-07-25 17:06:03 +0200

    add fontawesome

---
+++ src/fontawesome.js
@@ -0,0 +1,21 @@
+// import the library
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faCode,
+  faHighlighter,
+  faTools,
+  faCog,
+  faCogs,
+  faHome,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faCode,
+  faHighlighter,
+  faTools,
+  faCog,
+  faCogs,
+  faHome
+  // more icons go here
+);
