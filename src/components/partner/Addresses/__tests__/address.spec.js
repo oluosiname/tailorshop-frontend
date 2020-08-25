0762f22bb9901ef782c1cfd0b6c8b444e028199b
Olumuyiwa <oluosiname@gmail.com>
2020-08-25 18:49:55 +0200

    add address component test

--- src/components/partner/Addresses/__tests__/address.spec.js
+++ src/components/partner/Addresses/__tests__/address.spec.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Address from "../Address";
-import { render } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import renderer from "react-test-renderer";
+
+afterEach(cleanup);
+const address = {
+  uuid: "uuid",
+  address1: "Block 262",
+  address2: "Flat 10",
+  city: "Festac",
+  state: "Lagos",
+  primary: true,
+};
+
+const setPrimaryAddress = jest.fn((uuid) => {
+  return;
+});
+const deleteAddress = jest.fn((uuid) => {
+  return;
+});
 
 it("renderswithout crashing", () => {
   const div = document.createElement("div");
@@ -9,18 +27,38 @@ it("renderswithout crashing", () => {
   ReactDOM.render(<Address />, div);
 });
 
-it("renders without crashing", () => {
-  const address = {
-    uuid: "uuid",
-    address1: "Block 262",
-    address2: "Flat 10",
-    city: "Festac",
-    state: "Lagos",
-    primary: true,
-  };
-  const { getByTestId } = render(<Address address={address} />);
+it("renders properly", () => {
+  const { getByTestId } = render(
+    <Address
+      address={address}
+      setPrimaryAddress={setPrimaryAddress}
+      deleteAddress={deleteAddress}
+    />
+  );
   expect(getByTestId("address-address1")).toHaveTextContent("Block 262");
   expect(getByTestId("address-address2")).toHaveTextContent("Flat 10");
   expect(getByTestId("address-city")).toHaveTextContent("Festac");
   expect(getByTestId("address-state")).toHaveTextContent("Lagos");
+});
+
+it("matches snapshot", () => {
+  const tree = renderer.create(<Address address={address} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("calls delete function", () => {
+  const { getByTestId, getByText } = render(
+    <Address
+      address={address}
+      setPrimaryAddress={setPrimaryAddress}
+      deleteAddress={deleteAddress}
+    />
+  );
+  fireEvent.click(getByText("Delete"));
+  expect(deleteAddress).toHaveBeenCalledTimes(1);
+  expect(deleteAddress).toHaveBeenCalledWith("uuid");
+
+  fireEvent.click(getByTestId("toggle-primary-address"));
+  expect(setPrimaryAddress).toHaveBeenCalledTimes(1);
+  expect(setPrimaryAddress).toHaveBeenCalledWith("uuid");
 });
--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -3,7 +3,7 @@ import "./style.scss";
 
 const Toggle = ({ onChange, checked }) => {
   return (
-    <label className="toggle-control">
+    <label className="toggle-control" data-testid="toggle-primary-address">
       <input type="checkbox" checked={checked} onChange={onChange} />
       <span className="control"></span>
     </label>
