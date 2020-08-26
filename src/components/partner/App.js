98fbfc4611eee17557b9e173dd9985703ae9f9cf
Olumuyiwa <oluosiname@gmail.com>
2020-08-26 11:57:24 +0200

    redesign sidebar and nav

--- src/components/partner/App.js
+++ src/components/partner/App.js
@@ -1,50 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { NavLink, Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useLocation } from "react-router-dom";
 
 import "./app.scss";
+import Header from "./Header";
 const App = (props) => {
-  const [expanded, setExpanded] = useState("");
   const [showSideBar, setshowSideBar] = useState(false);
-  let location = useLocation();
-  const toggle = (toExpand) => {
-    if (expanded === toExpand) {
-      setExpanded("");
-    } else {
-      setExpanded(toExpand);
-    }
-  };
 
   useEffect(() => {
-    const name = location.pathname.split("/")[2];
-    setExpanded(name);
     setshowSideBar(false);
-  }, [location]);
-
-  const isExpanded = (name) => {
-    return expanded === name;
-  };
+  }, []);
 
   return (
     <div className="partner-app">
-      <header>
-        <FontAwesomeIcon
-          icon="ellipsis-v"
-          className="toggle-sidebar"
-          onClick={() => setshowSideBar((prev) => !prev)}
-        />{" "}
-      </header>
-
+      <Header setshowSideBar={setshowSideBar} showSideBar={showSideBar} />
       <main className="p-right-2 p-left-2">
-        <section className={`sidebar ${showSideBar ? "show" : ""}`}>
-          <div
-            className="sidebar__brand p-right-2 p-left-2 p-bottom-5 m-bottom-5"
-            style={{ border: "2px solid red", color: "black", height: "50px" }}
-          >
-            Tailorshop
-          </div>
+        {/* <section className={`sidebar ${showSideBar ? "show" : ""}`}>
           <div className="sidebar__container p-3">
+            <div
+              className="sidebar__brand p-right-2 p-left-2"
+              style={{
+                height: "50px",
+              }}
+            >
+              <img style={{ width: "50px" }} src={brand} alt="brand" />
+            </div>
             <FontAwesomeIcon
               icon="times"
               className="toggle-sidebar hide-sidebar clearfix "
@@ -54,57 +32,12 @@ const App = (props) => {
             <div className="sidebar__navigations">
               <NavLink exact to="/partner/dashboard" className="sidebar__link">
                 <FontAwesomeIcon icon="home" className="sidebar__nav-icon" />{" "}
-                Dashboard
+                Home
               </NavLink>
-              <div
-                className="sidebar__link"
-                name="jobs"
-                onClick={() => toggle("jobs")}
-              >
+              <NavLink exact to="/partner/jobs" className="sidebar__link">
                 <FontAwesomeIcon icon="tasks" className="sidebar__nav-icon" />{" "}
-                <span>Jobs</span>
-                <FontAwesomeIcon
-                  icon={`${isExpanded("jobs") ? "angle-up" : "angle-down"}`}
-                />{" "}
-              </div>
-              <div
-                className={`sidebar-inner ${
-                  isExpanded("jobs") ? "show" : "hidden"
-                }`}
-              >
-                <NavLink
-                  exact
-                  to="/partner/jobs"
-                  className="sidebar-inner__link"
-                >
-                  <span className="sidebar-inner__subnav-icon bg-orange"></span>
-                  <span>All Jobs</span>
-                </NavLink>
-                <NavLink
-                  exact
-                  to="/partner/jobs/pending"
-                  className="sidebar-inner__link"
-                >
-                  <span className="sidebar-inner__subnav-icon bg-yellow"></span>
-                  <span>Pending Jobs</span>
-                </NavLink>
-                <NavLink
-                  exact
-                  to="/partner/jobs/completed"
-                  className="sidebar-inner__link"
-                >
-                  <span className="sidebar-inner__subnav-icon bg-green"></span>
-                  <span>Completed Jobs</span>
-                </NavLink>
-                <NavLink
-                  exact
-                  to="/partner/jobs/new"
-                  className="sidebar-inner__link"
-                >
-                  <span className="sidebar-inner__subnav-icon bg-red"></span>
-                  <span>New Job</span>
-                </NavLink>
-              </div>
+                Jobs
+              </NavLink>
 
               <NavLink exact to="/partner/clients" className="sidebar__link">
                 <FontAwesomeIcon icon="users" className="sidebar__nav-icon" />{" "}
@@ -146,7 +79,7 @@ const App = (props) => {
               </NavLink>
             </div>
           </div>
-        </section>
+        </section> */}
         <section className="content">{props.children}</section>
       </main>
       <div className={`overlay ${showSideBar ? "show" : ""}`}></div>
---
+++ src/components/partner/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+import logo from "../../images/logo.png";
+
+const Header = ({ setshowSideBar, showSideBar }) => {
+  return (
+    <React.Fragment>
+      <div className="mobile-header">
+        <FontAwesomeIcon
+          icon="bars"
+          className="toggle-sidebar"
+          onClick={() => setshowSideBar((prev) => !prev)}
+        />{" "}
+      </div>
+
+      <header className={`${showSideBar ? "show" : ""}`}>
+        <div className="flex container">
+          <div className="logo">
+            <img src={logo} alt="logo" style={{ height: "35px" }} />
+            <FontAwesomeIcon
+              icon="times"
+              className="toggle-sidebar hide-sidebar clearfix "
+              onClick={() => setshowSideBar((prev) => !prev)}
+            />{" "}
+          </div>
+          <nav>
+            <ul className="flex">
+              <li>
+                {" "}
+                <NavLink exact to="/partner/dashboard" className="nav-link">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-link">Jobs</li>
+              <li>
+                <NavLink exact to="/partner/clients" className="nav-link">
+                  Clients
+                </NavLink>
+              </li>
+              <li className="nav-link">Portfolio</li>
+              <li className="nav-link not-mobile">Account</li>
+              <li className="nav-link mobile">Profile</li>
+              <li className="nav-link mobile">Settings</li>
+              <li className="nav-link mobile">Sign Out</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </React.Fragment>
+  );
+};
+
+export default Header;
